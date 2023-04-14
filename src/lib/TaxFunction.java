e2632e667b81aa1aeb3d699e2cf90ff061285688
razqyaqil <razqyaqil2@gmail.com>
2023-04-14 21:20:08 +0700

    Mengubah Comment
    mengubah comment pada metode calculateTax

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -4,13 +4,15 @@ public class TaxFunction {
 
 	
 	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
+	 * Menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
 	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
+	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan 
+	 * total gaji selama bulan bekerja dikurangi penghasilan tidak kena pajak
 	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
+	 * penghasilan tidak kena pajaknya pegawai adalah Rp 54.000.000
+	 * jika pegawai menikah penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
+	 * jika pegawai memiliki anak pajaknya ditambah sebesar Rp 1.500.000/anak.
+	 * maksimal penambahan anak adalah 3.
 	 * 
 	 */
 	
