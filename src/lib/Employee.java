b765ade5bfb238900ec918ed5c46a1f81397c3bf
razqyaqil <razqyaqil2@gmail.com>
2023-04-14 21:10:43 +0700

    mengubah comment
    mengubah dan merapihkan comment

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -16,7 +16,7 @@ public class Employee {
 	
 	private boolean isForeigner;
 	private boolean isMarried;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private boolean gender; //Laki-laki=true, Perempuan=false
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -39,15 +39,15 @@ public class Employee {
 		childIdNumbers = new LinkedList<String>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya 
-		* grade 1: 3.000.000 per bulan, 
-		* grade 2: 5.000.000 per bulan, 
-		* grade 3: 7.000.000 per bulan
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {	
+
+	public void setMonthlySalary(int grade) {
+		/**
+		* Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya 
+			* grade 1: 3.000.000 per bulan, 
+			* grade 2: 5.000.000 per bulan, 
+			* grade 3: 7.000.000 per bulan
+		* Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
+		*/	
 		if (grade == 1) {
 			monthlySalary = 3000000;
 		}else if (grade == 2) {
@@ -73,8 +73,7 @@ public class Employee {
 		childIdNumbers.add(childIdNumber);
 	}
 	
-	public int getAnnualIncomeTax() {
-		
+	public int getAnnualIncomeTax() {	
 		/*
 		*Menghitung berapa lama pegawai bekerja dalam setahun ini, 
 		*jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
