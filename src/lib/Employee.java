10c62752e9862b9bb931642a76bbfa1547ac6319
razqyaqil <razqyaqil2@gmail.com>
2023-04-14 14:35:52 +0700

    mengubah method setMonthlySalary
    memindahkan percabangan isForeginer diluar agar tidak terjadi pengulangan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -47,19 +47,13 @@ public class Employee {
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
 			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
 		}else if (grade == 2) {
 			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
 		}else if (grade == 3) {
 			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		}
+		if (isForeigner) {
+			monthlySalary = (int) (monthlySalary * 1.5);
 		}
 	}
 	
