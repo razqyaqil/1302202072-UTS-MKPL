b1f7d48a869be41698adef80eb4e7af3b238c9c5
razqyaqil <razqyaqil2@gmail.com>
2023-04-14 21:27:30 +0700

    Menghapus code duplikat
    menghapus kode duplikat pada percabangan isMarried, dan membuat nilai awal tax menjadi tetap

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -19,24 +19,17 @@ public class TaxFunction {
 	
 	public static int calculateTax(int Salary, int deductible, boolean isMarried, int numberOfChildren) {
 		
-		int tax = 0;
-		
-		
+		int tax = (int) Math.round(0.05 * (Salary - deductible - 54000000));
+
 		if (numberOfChildren > 3) {
 			numberOfChildren = 3;
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(0.05 * (Salary - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (Salary - deductible - 54000000));
+			tax = tax + 4500000 + (numberOfChildren * 1500000);
 		}
 		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
+		return tax;
 			 
 	}
 	
